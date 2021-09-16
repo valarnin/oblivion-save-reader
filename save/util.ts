67828db29fe7b288209eac324e542b241c138c0c
valarnin <valarnin@gmail.com>
2021-09-16 17:28:26 -0400

    Comment out debug assertions in util.ts for now

--- save/util.ts
+++ save/util.ts
@@ -18,8 +18,8 @@ export class SaveBuffer {
 
     readDate(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 16 <= endOffset)) debugger;
-        console.assert(this.offset + 16 <= endOffset);
+        // if (!(this.offset + 16 <= endOffset)) debugger;
+        // console.assert(this.offset + 16 <= endOffset);
         const wordBuf = new Uint16Array(this.buffer.slice(this.offset, this.realOffset += 16));
         const wYear = wordBuf[0];
         const wMonth = wordBuf[1];
@@ -34,56 +34,56 @@ export class SaveBuffer {
 
     readInt(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 4 <= endOffset)) debugger;
-        console.assert(this.offset + 4 <= endOffset);
+        // if (!(this.offset + 4 <= endOffset)) debugger;
+        // console.assert(this.offset + 4 <= endOffset);
         const intBuf = new Uint32Array(this.buffer.slice(this.offset, this.realOffset += 4));
         return intBuf[0];
     }
 
     readShort(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 2 <= endOffset)) debugger;
-        console.assert(this.offset + 2 <= endOffset);
+        // if (!(this.offset + 2 <= endOffset)) debugger;
+        // console.assert(this.offset + 2 <= endOffset);
         const shortBuf = new Uint16Array(this.buffer.slice(this.offset, this.realOffset += 2));
         return shortBuf[0];
     }
 
     peekShort(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 2 <= endOffset)) debugger;
-        console.assert(this.offset + 2 <= endOffset);
+        // if (!(this.offset + 2 <= endOffset)) debugger;
+        // console.assert(this.offset + 2 <= endOffset);
         const shortBuf = new Uint16Array(this.buffer.slice(this.offset, this.realOffset + 2));
         return shortBuf[0];
     }
 
     readByte(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 1 <= endOffset)) debugger;
-        console.assert(this.offset + 1 <= endOffset);
+        // if (!(this.offset + 1 <= endOffset)) debugger;
+        // console.assert(this.offset + 1 <= endOffset);
         const byteBuf = new Uint8Array(this.buffer.slice(this.offset, this.realOffset += 1));
         return byteBuf[0];
     }
 
     peekByte(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 1 <= endOffset)) debugger;
-        console.assert(this.offset + 1 <= endOffset);
+        // if (!(this.offset + 1 <= endOffset)) debugger;
+        // console.assert(this.offset + 1 <= endOffset);
         const byteBuf = new Uint8Array(this.buffer.slice(this.offset, this.realOffset + 1));
         return byteBuf[0];
     }
 
     readFloat(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 4 <= endOffset)) debugger;
-        console.assert(this.offset + 4 <= endOffset);
+        // if (!(this.offset + 4 <= endOffset)) debugger;
+        // console.assert(this.offset + 4 <= endOffset);
         const floatBuf = new Float32Array(this.buffer.slice(this.offset, this.realOffset += 4));
         return floatBuf[0];
     }
 
     readDouble(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 8 <= endOffset)) debugger;
-        console.assert(this.offset + 8 <= endOffset);
+        // if (!(this.offset + 8 <= endOffset)) debugger;
+        // console.assert(this.offset + 8 <= endOffset);
         const doubleBuf = new Float64Array(this.buffer.slice(this.offset, this.realOffset += 8));
         return doubleBuf[0];
     }
@@ -98,12 +98,12 @@ export class SaveBuffer {
 
     readbString(endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + 1 <= endOffset)) debugger;
-        console.assert(this.offset + 1 <= endOffset);
+        // if (!(this.offset + 1 <= endOffset)) debugger;
+        // console.assert(this.offset + 1 <= endOffset);
         const strLen = this.readByte(endOffset);
 
-        if (!(this.offset + strLen <= endOffset)) debugger;
-        console.assert(this.offset + strLen <= endOffset);
+        // if (!(this.offset + strLen <= endOffset)) debugger;
+        // console.assert(this.offset + strLen <= endOffset);
         const str = this.readString(strLen, endOffset);
 
         return str;
@@ -111,43 +111,43 @@ export class SaveBuffer {
 
     readString(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + len <= endOffset)) debugger;
-        console.assert(this.offset + len <= endOffset);
+        // if (!(this.offset + len <= endOffset)) debugger;
+        // console.assert(this.offset + len <= endOffset);
         return String.fromCharCode(...new Uint8Array(this.buffer.slice(this.offset, this.realOffset += len)));
     }
 
     readByteArray(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + len <= endOffset)) debugger;
-        console.assert(this.offset + len <= endOffset);
+        // if (!(this.offset + len <= endOffset)) debugger;
+        // console.assert(this.offset + len <= endOffset);
         return [...new Uint8Array(this.buffer.slice(this.offset, this.realOffset += len))];
     }
 
     readShortArray(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + (len * 2) <= endOffset)) debugger;
-        console.assert(this.offset + (len * 2) <= endOffset);
+        // if (!(this.offset + (len * 2) <= endOffset)) debugger;
+        // console.assert(this.offset + (len * 2) <= endOffset);
         return [...new Uint16Array(this.buffer.slice(this.offset, this.realOffset += (len * 2)))];
     }
 
     readIntArray(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + (len * 4) <= endOffset)) debugger;
-        console.assert(this.offset + (len * 4) <= endOffset);
+        // if (!(this.offset + (len * 4) <= endOffset)) debugger;
+        // console.assert(this.offset + (len * 4) <= endOffset);
         return [...new Uint32Array(this.buffer.slice(this.offset, this.realOffset += (len * 4)))];
     }
 
     readFloatArray(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + (len * 4) <= endOffset)) debugger;
-        console.assert(this.offset + (len * 4) <= endOffset);
+        // if (!(this.offset + (len * 4) <= endOffset)) debugger;
+        // console.assert(this.offset + (len * 4) <= endOffset);
         return [...new Float32Array(this.buffer.slice(this.offset, this.realOffset += (len * 4)))];
     }
 
     readDoubleArray(len: number, endOffset?: number) {
         endOffset ??= this.buffer.byteLength;
-        if (!(this.offset + (len * 8) <= endOffset)) debugger;
-        console.assert(this.offset + (len * 8) <= endOffset);
+        // if (!(this.offset + (len * 8) <= endOffset)) debugger;
+        // console.assert(this.offset + (len * 8) <= endOffset);
         return [...new Float64Array(this.buffer.slice(this.offset, this.realOffset += (len * 8)))];
     }
 
